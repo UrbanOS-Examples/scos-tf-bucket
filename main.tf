a5fcbb185ff6f8cb549d235ab7c5986906645f7a
Benjamin Brewer <ben@benbrewer.me>
2020-07-07 15:33:17 -0400

    add standard security setup for buckets

---
+++ main.tf
@@ -0,0 +1,91 @@
+variable "name" {
+  description = "The name of the bucket to create"
+}
+
+variable "region" {
+  description = "The region in which to create the bucket"
+}
+
+variable "versioning" {
+  description = "Whether or not to enable versioning"
+  default     = false
+}
+
+variable "policy" {
+  description = "A policy to merge with the default bucket policy that requires SSL"
+  default     = ""
+}
+
+resource "aws_s3_bucket" "secure_bucket" {
+  region = "${var.region}"
+
+  bucket = "${var.name}"
+  acl    = "private"
+
+  versioning {
+    enabled = "${var.versioning}"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "secure_bucket" {
+  bucket = "${aws_s3_bucket.secure_bucket.id}"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+data "aws_iam_policy_document" "secure_bucket" {
+  override_json = "${var.policy}"
+
+  statement {
+    sid = "AllowSSLRequestsOnly"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    effect = "Deny"
+
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.secure_bucket.arn}",
+      "${aws_s3_bucket.secure_bucket.arn}/*",
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "secure_bucket" {
+  bucket = "${aws_s3_bucket.secure_bucket.id}"
+
+  policy = "${data.aws_iam_policy_document.secure_bucket.json}"
+}
+
+output "bucket_arn" {
+  description = "The ARN of the bucket created by this module"
+  value       = "${aws_s3_bucket.secure_bucket.arn}"
+}
+
+output "bucket_name" {
+  description = "The name of the bucket created by this module"
+  value       = "${aws_s3_bucket.secure_bucket.bucket}"
+}
